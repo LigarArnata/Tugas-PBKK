1a15a5124a304b49f3aa97535d94698e75841c48
Garrr <95403456+LigarArnata@users.noreply.github.com>
2023-09-25 19:52:57 +0700

    Add files via upload

---
+++ ListName/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace ListName
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddNameButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(NameBox.Text) && !NameList.Items.Contains(NameBox.Text))
+            {
+                NameList.Items.Add(NameBox.Text);
+                NameBox.Clear();
+            }
+        }
+    }
+}
